5d1384246912aa9349ed62c43f354ee731c31cac
Hoven <hovenyang.best@gmail.com>
2024-11-18 09:24:18 +0800

    fix: consul client init too fast

--- cores/puzzles/consul-puzzle/consul.go
+++ cores/puzzles/consul-puzzle/consul.go
@@ -5,27 +5,35 @@ import (
 	"fmt"
 	"sort"
 	"strings"
-	
+
 	"github.com/go-puzzles/puzzles/cores"
 	"github.com/go-puzzles/puzzles/cores/discover"
 	"github.com/go-puzzles/puzzles/cores/discover/consul"
 	"github.com/go-puzzles/puzzles/cores/share"
 	"github.com/go-puzzles/puzzles/pflags"
 	"github.com/go-puzzles/puzzles/plog"
+	"github.com/go-puzzles/puzzles/snail"
 	"github.com/pkg/errors"
 )
 
 type consulPuzzle struct {
 }
 
+var (
+	consulAddr = pflags.String("consulAddr", share.GetConsulAddr(), "Set the conusl addr.")
+)
+
 func init() {
-	share.ConsulAddr = pflags.String("consulAddr", share.GetConsulAddr(), "Set the conusl addr.")
+	share.ConsulAddr = consulAddr
+	snail.RegisterObject("setConsulClient", func() error {
+		discover.SetFinder(consul.GetConsulClient())
+		return nil
+	})
 }
 
 func WithConsulRegister() cores.ServiceOption {
 	return func(o *cores.Options) {
 		o.RegisterPuzzle(&consulPuzzle{})
-		discover.SetFinder(consul.GetConsulClient())
 	}
 }
 
@@ -37,21 +45,21 @@ func (cp *consulPuzzle) StartPuzzle(ctx context.Context, opt *cores.Options) err
 	if opt.ListenerAddr == "" {
 		return errors.New("consul register handler can only be used when the service is listening on a port")
 	}
-	
+
 	if opt.ServiceName == "" {
 		return errors.New("consul register handler need a ServiceName to register")
 	}
-	
+
 	if len(opt.Tags) == 0 {
 		opt.Tags = append(opt.Tags, "dev")
 	}
-	
+
 	tags := opt.Tags[:]
 	sort.Strings(tags)
 	if err := discover.GetServiceFinder().RegisterServiceWithTags(opt.ServiceName, opt.ListenerAddr, tags); err != nil {
 		return errors.Wrap(err, "registerConsul")
 	}
-	
+
 	var logArgs []any
 	logText := "Registered into consul(%s) success. Service=%v"
 	logArgs = append(logArgs, consul.GetConsulAddress(), opt.ServiceName)
@@ -59,11 +67,11 @@ func (cp *consulPuzzle) StartPuzzle(ctx context.Context, opt *cores.Options) err
 		logText = fmt.Sprintf("%v %v", logText, "Tag=%v")
 		logArgs = append(logArgs, strings.Join(tags, ","))
 	}
-	
+
 	plog.Infoc(ctx, logText, logArgs...)
-	
+
 	<-ctx.Done()
-	
+
 	discover.GetServiceFinder().Close()
 	return nil
 }
